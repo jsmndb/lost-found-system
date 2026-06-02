157f09162d05c468bdbab39de6660e42d2848db2
jsmndb <jasmindbernal@gmail.com>
2026-06-02 11:44:48 +0800

    2nd Day

--- server/index.js
+++ server/index.js
@@ -1,30 +1,110 @@
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const db = require("./db");
 
 const app = express();
-
-const mysql = require("mysql2");
-
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "lost_found"
-});
-
-db.connect((err) => {
-    if (err) {
-        console.log("Database connection failed:", err);
-    } else {
-        console.log("Connected to MySQL (XAMPP)");
-    }
-});
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Lost and Found API is running");
+});
+
+app.post("/register", (req, res) => {
+    const { name, email, password } = req.body;
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    const sql =
+        "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+
+    db.query(
+        sql,
+        [name, email, hashedPassword],
+        (err, result) => {
+            if (err) {
+                return res.json(err);
+            }
+
+            res.json({
+                message: "User registered successfully"
+            });
+        }
+    );
+});
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+
+    const sql = "SELECT * FROM users WHERE email = ?";
+
+    db.query(sql, [email], (err, result) => {
+        if (err) {
+            return res.json({ error: err });
+        }
+
+        if (result.length === 0) {
+            return res.json({
+                message: "User not found",
+            });
+        }
+
+        const user = result[0];
+
+        const isMatch = bcrypt.compareSync(
+            password,
+            user.password
+        );
+
+        if (!isMatch) {
+            return res.json({
+                message: "Wrong password",
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+            },
+            "secretkey"
+        );
+
+        res.json({
+            message: "Login successful",
+            token,
+        });
+    });
+});
+
+app.get("/users", (req, res) => {
+    db.query("SELECT * FROM users", (err, result) => {
+        if (err) {
+            return res.json(err);
+        }
+
+        res.json(result);
+    });
+});
+
+app.post("/register", (req, res) => {
+    const { name, email, password } = req.body;
+
+    const sql =
+        "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+
+    db.query(sql, [name, email, password], (err, result) => {
+        if (err) {
+            return res.json(err);
+        }
+
+        res.json({
+            message: "User registered successfully"
+        });
+    });
 });
 
 app.listen(5000, () => {
